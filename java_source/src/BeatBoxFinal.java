62e37a390a9160c3b38bd470fee7074dafdffe06
JerryHan88 <jerryhan88@gmail.com>
2015-12-07 20:38:50 +0900

    BeatBox

---
+++ java_source/src/BeatBoxFinal.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
+import javax.sound.midi.*;
+import java.util.*;
+import java.net.*;
+import javax.swing.*;
+
+public class BeatBoxFinal {
+	JFrame theFrame;
+	JPanel mainPanel;
+	JList incomingList;
+	JTextField userMessage;
+	ArrayList<JCheckBox> checkboxList;
+	
+}
